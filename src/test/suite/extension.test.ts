55d0fc3148aa4f49674df560fb9c8a255f57159a
Aurélien Pupier <apupier@redhat.com>
2020-10-02 14:52:02 +0200

    tryign to get back to avoid suites and it is also failing...

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -23,103 +23,96 @@ import * as config from '../../config';
 import * as utils from './Utils';
 import { ACTIVATION_TIMEOUT } from './Utils';
 
-suite('Extension entry point test:', function() {
+suite('Extension entry point test:', function () {
 	const extensionId = 'redhat.vscode-camelk';
 
-	suite('test extension availability', function() {
-		test('vscode-camelk extension should be present', function() {
-			assert.ok(vscode.extensions.getExtension(extensionId));
-		});
-
-		test('vscode-camelk extension should activate', async() => {
-			await utils.ensureExtensionActivated();
-		}).timeout(ACTIVATION_TIMEOUT + 1000);
+	test('vscode-camelk extension should be present', function () {
+		assert.ok(vscode.extensions.getExtension(extensionId));
 	});
 
-	suite('test getBaseCmd', function() {
-		test('test that getBaseCmd returned value doesn\'t contain --namespace parameter when no namespace is passed', function () {
-			let cmdStrNoNS: string = kamel.getBaseCmd(`fakepath`, `fakecommand`, undefined);
-			assert.equal(cmdStrNoNS.indexOf('--namespace'), -1);
-		});
+	test('vscode-camelk extension should activate', async () => {
+		await utils.ensureExtensionActivated();
+	}).timeout(ACTIVATION_TIMEOUT + 1000);
 
-		test('test that getBaseCmd returned value contains --namespace parameter when namespace is passed', function () {
-			let cmdStrWithNS: string = kamel.getBaseCmd(`fakepath`, `fakecommand`, 'fakens');
-			assert.ok(cmdStrWithNS.includes('--namespace'));
-		});
+	test('test that getBaseCmd returned value doesn\'t contain --namespace parameter when no namespace is passed', function () {
+		let cmdStrNoNS: string = kamel.getBaseCmd(`fakepath`, `fakecommand`, undefined);
+		assert.equal(cmdStrNoNS.indexOf('--namespace'), -1);
 	});
 
-	suite('test settings', function() {
-		
-		let initialRuntimeVersion :string | undefined;
-		let initialNamespace :string | undefined;
-		let initialAutoUpgrade :boolean;
-		
-		this.beforeAll(async() => {
-			initialRuntimeVersion = config.getKamelRuntimeVersionConfig();
-			initialNamespace = config.getNamespaceconfig();
-			initialAutoUpgrade = config.getKamelAutoupgradeConfig();
+	test('test that getBaseCmd returned value contains --namespace parameter when namespace is passed', function () {
+		let cmdStrWithNS: string = kamel.getBaseCmd(`fakepath`, `fakecommand`, 'fakens');
+		assert.ok(cmdStrWithNS.includes('--namespace'));
+	});
+
+	let initialRuntimeVersion: string | undefined;
+	let initialNamespace: string | undefined;
+	let initialAutoUpgrade: boolean;
+
+	this.beforeAll(async () => {
+		initialRuntimeVersion = config.getKamelRuntimeVersionConfig();
+		initialNamespace = config.getNamespaceconfig();
+		initialAutoUpgrade = config.getKamelAutoupgradeConfig();
+	});
+
+	this.afterAll(async () => {
+		await config.setKamelRuntimeVersionConfig(initialRuntimeVersion as string);
+		await config.addNamespaceToConfig(initialNamespace);
+		await config.setKamelAutoupgradeConfig(initialAutoUpgrade);
+	});
+
+	test('test setting namespace to undefined', async function () {
+		const namespace: string | undefined = config.getNamespaceconfig();
+		await config.addNamespaceToConfig(undefined);
+
+		const namespaceConfigValueAfterReset: string | undefined = config.getNamespaceconfig();
+		assert.equal(namespaceConfigValueAfterReset, undefined, 'By default, the namespace config should be undefined');
+
+		await config.addNamespaceToConfig(namespace);
+	});
+
+	test('test setting namespace to other value', async function () {
+		const namespace: string | undefined = config.getNamespaceconfig();
+		const testNs = 'testing';
+		await config.addNamespaceToConfig(testNs);
+
+		const namespaceConfigValueAfterReset: string | undefined = config.getNamespaceconfig();
+		assert.equal(namespaceConfigValueAfterReset, testNs, 'The value specified should be available.');
+
+		await config.addNamespaceToConfig(namespace);
+	});
+
+	test('test can set autoupgrade setting', async function () {
+		await config.setKamelAutoupgradeConfig(true);
+
+		const autoConfigValue = config.getKamelAutoupgradeConfig();
+		assert.ok(autoConfigValue);
+
+		await config.setKamelAutoupgradeConfig(false);
+		const autoConfigValue2 = config.getKamelAutoupgradeConfig();
+		assert.isFalse(autoConfigValue2);
+	});
+
+	test('test can set runtime version setting', async function () {
+		await config.setKamelAutoupgradeConfig(false);
+		const autoConfigValue2 = config.getKamelAutoupgradeConfig();
+		assert.isFalse(autoConfigValue2);
+
+		const invalidVersion = 'invalidVersion';
+		await config.setKamelRuntimeVersionConfig(invalidVersion).then(() => {
+			const firstVersion = config.getKamelRuntimeVersionConfig();
+			assert.equal(firstVersion, invalidVersion);
+		}).catch((error) => {
+			assert.fail(error);
 		});
-			
-		this.afterAll(async() => {
-			await config.setKamelRuntimeVersionConfig(initialRuntimeVersion as string);
-			await config.addNamespaceToConfig(initialNamespace);
-			await config.setKamelAutoupgradeConfig(initialAutoUpgrade);
-		});
-		
-		test('test setting namespace to undefined', async function () {
-			const namespace: string | undefined = config.getNamespaceconfig();
-			await config.addNamespaceToConfig(undefined);
 
-			const namespaceConfigValueAfterReset: string | undefined = config.getNamespaceconfig();
-			assert.equal(namespaceConfigValueAfterReset, undefined, 'By default, the namespace config should be undefined');
-
-			await config.addNamespaceToConfig(namespace);
+		const validVersion = '1.0.0-RC1';
+		await config.setKamelRuntimeVersionConfig(validVersion).then(() => {
+			const secondVersion = config.getKamelRuntimeVersionConfig();
+			assert.equal(secondVersion, validVersion);
+		}).catch((error) => {
+			assert.fail(error);
 		});
 
-		test('test setting namespace to other value', async function () {
-			const namespace: string | undefined = config.getNamespaceconfig();
-			const testNs = 'testing';
-			await config.addNamespaceToConfig(testNs);
-
-			const namespaceConfigValueAfterReset: string | undefined = config.getNamespaceconfig();
-			assert.equal(namespaceConfigValueAfterReset, testNs, 'The value specified should be available.');
-
-			await config.addNamespaceToConfig(namespace);
-		});
-
-		test('test can set autoupgrade setting', async function () {
-			await config.setKamelAutoupgradeConfig(true);
-
-			const autoConfigValue = config.getKamelAutoupgradeConfig();
-			assert.ok(autoConfigValue);
-
-			await config.setKamelAutoupgradeConfig(false);
-			const autoConfigValue2 = config.getKamelAutoupgradeConfig();
-			assert.isFalse(autoConfigValue2);
-		});
-
-		test('test can set runtime version setting', async function () {
-			await config.setKamelAutoupgradeConfig(false);
-			const autoConfigValue2 = config.getKamelAutoupgradeConfig();
-			assert.isFalse(autoConfigValue2);
-
-			const invalidVersion = 'invalidVersion';
-			await config.setKamelRuntimeVersionConfig(invalidVersion).then(() => {
-				const firstVersion = config.getKamelRuntimeVersionConfig();
-				assert.equal(firstVersion, invalidVersion);
-			}).catch((error) => {
-				assert.fail(error);
-			});
-
-			const validVersion = '1.0.0-RC1';
-			await config.setKamelRuntimeVersionConfig(validVersion).then(() => {
-				const secondVersion = config.getKamelRuntimeVersionConfig();
-				assert.equal(secondVersion, validVersion);
-			}).catch((error) => {
-				assert.fail(error);
-			});
-
-			await config.setKamelAutoupgradeConfig(true);
-		});
+		await config.setKamelAutoupgradeConfig(true);
 	});
 });
